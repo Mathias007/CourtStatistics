8deb93b46301d13a03014e6fbbcfd7bde5a27598
Mathias007 <mathias.najlepszy@gmail.com>
2024-11-11 20:43:04 +0100

    [BE] Create Court Service.

---
+++ server/api/services/Court.service.ts
@@ -0,0 +1,31 @@
+import Court, { CourtDocument } from "../models/Court.model";
+
+export const getCourts = async (): Promise<CourtDocument[]> => {
+    return Court.find();
+};
+
+export const getCourtById = async (
+    id: string
+): Promise<CourtDocument | null> => {
+    return Court.findById(id);
+};
+
+export const createCourt = async (
+    data: CourtDocument
+): Promise<CourtDocument> => {
+    const court = new Court(data);
+    return court.save();
+};
+
+export const updateCourt = async (
+    id: string,
+    data: Partial<CourtDocument>
+): Promise<CourtDocument | null> => {
+    return Court.findByIdAndUpdate(id, data, { new: true });
+};
+
+export const deleteCourt = async (
+    id: string
+): Promise<CourtDocument | null> => {
+    return Court.findByIdAndDelete(id) as unknown as CourtDocument | null;
+};
