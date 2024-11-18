dc0a25c66028bad764c543ccc9688e4b3fad762b
Mathias007 <mathias.najlepszy@gmail.com>
2024-11-18 20:23:08 +0100

    [FE] Rebuild and extend CourtList.

--- client/src/components/CourtList.tsx
+++ client/src/components/CourtList.tsx
@@ -1,30 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { getCourts } from "../services/court.service";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Court } from "../models/court.model";
+import { getCourts, deleteCourt } from "../services/court.service";
 
 const CourtList: React.FC = () => {
     const [courts, setCourts] = useState<Court[]>([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchCourts = async () => {
             const data = await getCourts();
             setCourts(data);
         };
-
         fetchCourts();
     }, []);
 
     return (
         <div>
-            <h1>Courts</h1>
-            <ul>
-                {courts.map((court) => (
-                    <li key={court._id}>
-                        {court.court_name} - {court.statistics.length}{" "}
-                        statistics
-                    </li>
-                ))}
-            </ul>
+            <h1>Lista sądów</h1>
+            <button onClick={() => navigate("/courts/add")}>Dodaj sąd</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nazwa</th>
+                        {/* <th>Adres</th> */}
+                        <th>Statystyki</th>
+                        <th>Akcje</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {courts.map((court) => (
+                        <tr key={court._id}>
+                            <td>{court.court_name}</td>
+                            {/* <td>{court.address}</td> */}
+                            <td>{court.statistics.length}</td>
+                            <td>
+                                <button
+                                    onClick={() =>
+                                        navigate(`/courts/${court._id}`)
+                                    }
+                                >
+                                    Zobacz
+                                </button>
+                                <button
+                                    onClick={() =>
+                                        navigate(`/courts/edit/${court._id}`)
+                                    }
+                                >
+                                    Edytuj
+                                </button>
+                                <button onClick={() => deleteCourt(court._id)}>
+                                    Usuń
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
