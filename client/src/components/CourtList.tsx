945b3848f75eed7cd7ed3eff81ee5ba3c520ce61
Mathias007 <mathias.najlepszy@gmail.com>
2024-11-23 23:06:57 +0100

    [FE] Update and improve CourtList.

--- client/src/components/CourtList.tsx
+++ client/src/components/CourtList.tsx
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Court } from "../models/court.model";
 import { getCourts, deleteCourt } from "../services/court.service";
-import ComparativeBarChart from "./charts/ComparativeBarChart";
-import ResolvedCasesComparisonChart from "./charts/ResolvedCasesComparisionChart";
+
+import PieChartForCourts from "./charts/CourtsPieComparision";
+import ToggleSection from "./ToggleSection";
+import IncomeCasesComparision from "./charts/IncomeCasesComparision";
+import ResolvedCasesComparison from "./charts/ResolvedCasesComparision";
 
 const CourtList: React.FC = () => {
     const [courts, setCourts] = useState<Court[]>([]);
@@ -25,56 +28,70 @@ const CourtList: React.FC = () => {
     if (!courts.length) return <p>Wczytywanie...</p>;
 
     return (
-        <div>
+        <div className="dashboard-wrapper">
             <h1>Lista sądów</h1>
-            <button onClick={() => navigate("/courts/add")}>Dodaj sąd</button>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Nazwa</th>
-                        <th>Adres</th>
-                        <th>Statystyki</th>
-                        <th>Akcje</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {courts.map((court) => (
-                        <tr key={court._id} className="table-row">
-                            <td>{court.court_name}</td>
-                            <td>{court.court_address}</td>
-                            <td>{court.statistics.length}</td>
-                            <td className="table-actions">
-                                <button
-                                    className="table-button"
-                                    onClick={() =>
-                                        navigate(`/courts/${court._id}`)
-                                    }
-                                >
-                                    Zobacz
-                                </button>
-                                <button
-                                    className="table-button"
-                                    onClick={() =>
-                                        navigate(`/courts/edit/${court._id}`)
-                                    }
-                                >
-                                    Edytuj
-                                </button>
-                                <button
-                                    className="table-button"
-                                    onClick={() => handleDelete(court._id)}
-                                >
-                                    Usuń
-                                </button>
-                            </td>
+            <ToggleSection title="Tabela">
+                <div className="table-buttons">
+                    <button
+                        className="form-button"
+                        onClick={() => navigate("/courts/add")}
+                    >
+                        Dodaj sąd
+                    </button>
+                </div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Nazwa</th>
+                            <th>Adres</th>
+                            <th>Statystyki</th>
+                            <th>Operacje</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div className="comparative-chart">
-                <ComparativeBarChart />
-                <ResolvedCasesComparisonChart />
-            </div>
+                    </thead>
+                    <tbody>
+                        {courts.map((court) => (
+                            <tr key={court._id} className="table-row">
+                                <td>{court.court_name}</td>
+                                <td>{court.court_address}</td>
+                                <td>{court.statistics.length}</td>
+                                <td className="table-actions">
+                                    <button
+                                        className="table-button"
+                                        onClick={() =>
+                                            navigate(`/courts/${court._id}`)
+                                        }
+                                    >
+                                        Zobacz
+                                    </button>
+                                    <button
+                                        className="table-button"
+                                        onClick={() =>
+                                            navigate(
+                                                `/courts/edit/${court._id}`
+                                            )
+                                        }
+                                    >
+                                        Edytuj
+                                    </button>
+                                    <button
+                                        className="table-button"
+                                        onClick={() => handleDelete(court._id)}
+                                    >
+                                        Usuń
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </ToggleSection>
+            <ToggleSection title="Wykresy">
+                <div className="charts-section">
+                    <IncomeCasesComparision courts={courts} />
+                    <ResolvedCasesComparison courts={courts} />
+                    <PieChartForCourts courts={courts} />
+                </div>
+            </ToggleSection>
         </div>
     );
 };
