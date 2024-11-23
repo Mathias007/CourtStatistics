468169c8ebf51476b64e78b0a097aca7f62dc536
Mathias007 <mathias.najlepszy@gmail.com>
2024-11-23 23:07:03 +0100

    [FE] Update and improve CourtDetails.

--- client/src/components/CourtDetails.tsx
+++ client/src/components/CourtDetails.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Court } from "../models/court.model";
 import { getCourtById, deleteStatistic } from "../services/court.service";
+
 import BarChart from "./charts/BarChart";
 import CategoryChart from "./charts/CategoryChart";
 import ResolvedCasesChart from "./charts/ResolvedCasesChart";
+import ToggleSection from "./ToggleSection";
 
 const CourtDetails: React.FC = () => {
     const { id } = useParams<{ id: string }>();
@@ -36,71 +38,90 @@ const CourtDetails: React.FC = () => {
     if (!court) return <p>Wczytywanie...</p>;
 
     return (
-        <div>
+        <div className="dashboard-wrapper">
             <h1>{court.court_name}</h1>
-            <p>Adres: {court.court_address}</p>
-            <BarChart courtId={court._id} />
-            <ResolvedCasesChart courtId={court._id} />
-            <CategoryChart courtId={court._id} />
-
-            <h2>Statystyki</h2>
-            <button
-                onClick={() => navigate(`/courts/${court._id}/statistics/add`)}
-                className="form-button"
-            >
-                Dodaj statystykę
-            </button>
-            <button onClick={() => navigate("/")} className="form-button">
-                Wróć
-            </button>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Rok</th>
-                        <th>Kategoria</th>
-                        <th>Zaległość wejściowa</th>
-                        <th>Wpływ</th>
-                        <th>Załatwienia</th>
-                        <th>Zaległość wyjściowa</th>
-                        <th>Akcje</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {court.statistics.map((stat) => (
-                        <tr key={stat.id} className="table-row">
-                            <td>{stat.year}</td>
-                            <td>{stat.category}</td>
-                            <td>{stat.backlog_start}</td>
-                            <td>{stat.incoming}</td>
-                            <td>{stat.resolved}</td>
-                            <td>{stat.backlog_end}</td>
-                            <td className="table-actions">
-                                <button
-                                    className="table-button"
-                                    onClick={() =>
-                                        navigate(
-                                            `/courts/${court._id}/statistics/edit/${stat.id}`
-                                        )
-                                    }
-                                >
-                                    Edytuj
-                                </button>
-                                <button
-                                    className="table-button"
-                                    onClick={() =>
-                                        handleDeleteStatistic(
-                                            court._id,
-                                            stat.id
-                                        )
-                                    }
-                                >
-                                    Usuń
-                                </button>
-                            </td>
+            <p>{court.court_address}</p>
+            <ToggleSection title="Statystyki">
+                <div className="table-buttons">
+                    <button
+                        onClick={() =>
+                            navigate(`/courts/${court._id}/statistics/add`)
+                        }
+                        className="form-button"
+                    >
+                        Dodaj statystykę
+                    </button>
+                    <button
+                        onClick={() => navigate("/")}
+                        className="form-button"
+                    >
+                        Wróć
+                    </button>
+                </div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Rok</th>
+                            <th>Kategoria spraw</th>
+                            <th>Zaległość wejściowa</th>
+                            <th>Wpływ</th>
+                            <th>Załatwienia</th>
+                            <th>Zaległość wyjściowa</th>
+                            <th>Operacje</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {court.statistics.map((stat) => (
+                            <tr key={stat.id} className="table-row">
+                                <td>{stat.year}</td>
+                                <td>
+                                    {stat.category === "PENAL"
+                                        ? "Prawo Karne"
+                                        : stat.category === "CIVIL"
+                                        ? "Prawo Cywilne"
+                                        : stat.category === "LABOR"
+                                        ? "Prawo Pracy"
+                                        : "Nieznana"}
+                                </td>
+                                <td>{stat.backlog_start}</td>
+                                <td>{stat.incoming}</td>
+                                <td>{stat.resolved}</td>
+                                <td>{stat.backlog_end}</td>
+                                <td className="table-actions">
+                                    <button
+                                        className="table-button"
+                                        onClick={() =>
+                                            navigate(
+                                                `/courts/${court._id}/statistics/edit/${stat.id}`
+                                            )
+                                        }
+                                    >
+                                        Edytuj
+                                    </button>
+                                    <button
+                                        className="table-button"
+                                        onClick={() =>
+                                            handleDeleteStatistic(
+                                                court._id,
+                                                stat.id
+                                            )
+                                        }
+                                    >
+                                        Usuń
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </ToggleSection>
+            <ToggleSection title="Wykresy">
+                <div className="charts-section">
+                    <BarChart courtId={court._id} />
+                    <ResolvedCasesChart courtId={court._id} />
+                    <CategoryChart courtId={court._id} />
+                </div>
+            </ToggleSection>
         </div>
     );
 };
