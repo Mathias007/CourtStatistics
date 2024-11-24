cc72c82cf18914d2b8c82a2d9aa74dfed903cd1d
Mathias007 <mathias.najlepszy@gmail.com>
2024-11-24 19:18:17 +0100

    [BE] Prepare User model.

---
+++ server/api/models/User.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcryptjs";
+
+import { DatabaseModels } from "../../config/DatabaseModels";
+import { BCRYPT_SALT_ROUNDS } from "../../config/ConfigVariables";
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+export interface UserDocument extends Document {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+    {
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+    },
+    { timestamps: true }
+);
+
+UserSchema.pre<IUser>("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(BCRYPT_SALT_ROUNDS);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.comparePassword = function (
+    candidatePassword: string
+): Promise<boolean> {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User = mongoose.model<IUser>(DatabaseModels.USER, UserSchema);
