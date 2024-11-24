0400701d5677575ec294ae79c0c6f5846b6657c0
Mathias007 <mathias.najlepszy@gmail.com>
2024-11-24 19:21:24 +0100

    [FE] Create LogoutButton component.

---
+++ client/src/components/LogoutButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../services/user.service";
+
+const LogoutButton: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/login");
+    };
+
+    return <button onClick={handleLogout}>Wyloguj się</button>;
+};
+
+export default LogoutButton;
