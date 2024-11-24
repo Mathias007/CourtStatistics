842761e7e35d342ea8457b24959dd6677dfe8510
Mathias007 <mathias.najlepszy@gmail.com>
2024-11-24 19:19:43 +0100

    [BE] Implement Auth middleware in Court route.

--- server/api/routes/Court.route.ts
+++ server/api/routes/Court.route.ts
@@ -1,25 +1,33 @@
 import express from "express";
 import * as courtController from "../controllers/Court.controller";
+import { authMiddleware } from "../middlewares/Auth.middleware";
 
 const router = express.Router();
 
-router.get("/", courtController.getCourts);
-router.get("/:id", courtController.getCourtById);
-router.post("/", courtController.createCourt);
-router.put("/:id", courtController.updateCourt);
-router.delete("/:id", courtController.deleteCourt);
+router.get("/", authMiddleware, courtController.getCourts);
+router.get("/:id", authMiddleware, courtController.getCourtById);
+router.post("/", authMiddleware, courtController.createCourt);
+router.put("/:id", authMiddleware, courtController.updateCourt);
+router.delete("/:id", authMiddleware, courtController.deleteCourt);
 
 router.get(
     "/:courtId/statistics/:year/:category",
+    authMiddleware,
     courtController.getStatisticsByCourt
 );
-router.post("/:courtId/statistics", courtController.addStatisticToCourt);
+router.post(
+    "/:courtId/statistics",
+    authMiddleware,
+    courtController.addStatisticToCourt
+);
 router.put(
     "/:courtId/statistics/:statisticId",
+    authMiddleware,
     courtController.updateStatisticById
 );
 router.delete(
     "/:courtId/statistics/:statisticId",
+    authMiddleware,
     courtController.deleteStatisticById
 );
 
